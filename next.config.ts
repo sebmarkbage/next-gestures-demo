15c461924739aefe9f9f49bc751616cce9825892
Sebastian Markbage <sebastian@calyptus.eu>
2026-01-20 20:59:28 -0500

    Re-enable Cache Components
    I think it got fixed by canceling the gesture on unmount.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     gestureTransition: true,
-    cacheComponents: false,
+    cacheComponents: true,
   },
 };
 
